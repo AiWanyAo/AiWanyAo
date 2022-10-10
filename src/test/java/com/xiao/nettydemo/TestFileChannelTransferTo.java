ae0e41ff80c1b770a112579482c7df22100a991f
Xiao <2201086800@qq.com>
2022-10-10 19:35:07 +0800

    第一次提交 {   框架 ：Netty,   UP ： 黑马,   P : 18 }

---
+++ src/test/java/com/xiao/nettydemo/TestFileChannelTransferTo.java
@@ -0,0 +1,27 @@
+package com.xiao.nettydemo;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+public class TestFileChannelTransferTo {
+
+    // channel 数据传输
+    public static void main(String[] args) {
+        try (
+                FileChannel from = new FileInputStream("data.txt").getChannel();
+                FileChannel to =new FileOutputStream("to.txt").getChannel();
+        ) {
+            // 效率高， 底层会利用操作系统的零拷贝进行优化, 2g 数据
+            long size = from.size();
+            for (long left = size; left>0;){
+                // left 变量代表还剩余多少字节
+                left -= from.transferTo((size - left), size,to);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/test/java/com/xiao/nettydemo/TestScatteringReads.java
@@ -0,0 +1,25 @@
+package com.xiao.nettydemo;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+import static com.xiao.nettydemo.ByteBufferUtil.debugAll;
+
+public class TestScatteringReads {
+
+    public static void main(String[] args) {
+        try (FileChannel channel = new RandomAccessFile("words.txt", "r").getChannel()) {
+            ByteBuffer b1 = ByteBuffer.allocate(3);
+            ByteBuffer b2 = ByteBuffer.allocate(3);
+            ByteBuffer b3 = ByteBuffer.allocate(3);
+            channel.read(new ByteBuffer[]{b1,b2,b3});
+            debugAll(b1);
+            debugAll(b2);
+            debugAll(b3);
+        } catch (IOException e) {
+        }
+    }
+
+}
