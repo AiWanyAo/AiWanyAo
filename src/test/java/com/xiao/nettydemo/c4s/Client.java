a10494ba6c7684f83981a057b4b85413fb03cadb
Xiao <2201086800@qq.com>
2022-10-14 02:42:52 +0800

    第三次提交 {   框架 ：Netty,   UP ： 黑马,   P : 40 }

--- src/test/java/com/xiao/nettydemo/c4s/Client.java
+++ src/test/java/com/xiao/nettydemo/c4s/Client.java
@@ -2,14 +2,18 @@ package com.xiao.nettydemo.c4s;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 
 public class Client {
 
     public static void main(String[] args) throws IOException {
         SocketChannel sc = SocketChannel.open();
         sc.connect(new InetSocketAddress("localhost",8080));
-        System.out.println("waiting....");
+        SocketAddress address = sc.getLocalAddress();
+        sc.write(Charset.defaultCharset().encode("01234567890abcdef3333\n"));
+        System.in.read();
     }
 
 }
--- src/test/java/com/xiao/nettydemo/c4sss/Server.java
+++ src/test/java/com/xiao/nettydemo/c4sss/Server.java
@@ -1,21 +1,22 @@
-package com.xiao.nettydemo.c4ss;
+package com.xiao.nettydemo.c4sss;
 
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.*;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
-import static com.xiao.nettydemo.ByteBufferUtil.debugRead;
+import static com.xiao.nettydemo.ByteBufferUtil.debugAll;
 
 @Slf4j
 public class Server {
-
+    // 处理消息边界
     public static void main(String[] args) throws IOException {
         // 1. 创建 selector, 管理多个 channel
         Selector selector = Selector.open();
@@ -46,19 +47,26 @@ public class Server {
                     ServerSocketChannel channel = (ServerSocketChannel) key.channel();
                     SocketChannel sc = channel.accept();
                     sc.configureBlocking(false);
-                    SelectionKey scKey = sc.register(selector, 0, null);
+                    ByteBuffer buffer = ByteBuffer.allocate(16); // attachment
+                    // 将一个 ByteBuffer 作为附件关联到 selectionKey 上
+                    SelectionKey scKey = sc.register(selector, 0, buffer);
                     scKey.interestOps(SelectionKey.OP_READ);
                     log.debug("{}:",sc);
                 }else if (key.isReadable()){    // 如果是 read
                     try {
                         SocketChannel channel = (SocketChannel)key.channel(); // 拿到触发事件的channel\
-                        ByteBuffer buffer = ByteBuffer.allocate(4);
+                        ByteBuffer buffer = (ByteBuffer) key.attachment();
                         int read = channel.read(buffer); // 如果是正常断开， read 的方法的返回值是 -1
                         if (read == -1){
                             key.cancel();
                         } else {
-                            buffer.flip();
-                            debugRead(buffer);
+                            split(buffer);
+                            if (buffer.position() == buffer.limit()){
+                                ByteBuffer newBuffer = ByteBuffer.allocate(buffer.capacity() * 2);
+                                buffer.flip();
+                                newBuffer.put(buffer); // 01234567890abcdef
+                                key.attach(newBuffer);
+                            }
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -69,10 +77,27 @@ public class Server {
                 iter.remove();
                 // 取消
 //                key.cancel();
-
             }
         }
 
+    }
+
+
+    private static void split(ByteBuffer source) {
+        source.flip();
+        for (int i = 0; i <source.limit(); i++) {
+            if (source.get(i) == '\n') {
+                int length = i + 1 - source.position();
+                // 把这条完整消息存入新的ByteBuffer
+                ByteBuffer target = ByteBuffer.allocate(length);
+                // 从 source 读， 向 target 写
+                for (int j = 0; j < length; j++) {
+                    target.put(source.get());
+                }
+                debugAll(target);
+            }
+        }
+        source.compact(); // 01234567890abcdef
     }
 
 }
