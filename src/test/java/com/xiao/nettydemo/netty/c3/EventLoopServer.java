b8f1515bfdbf08bf697b8c4ce62061ce95fe400e
Xiao <2201086800@qq.com>
2022-10-20 03:02:01 +0800

    第六次提交 {   框架 ：Netty,   UP ： 黑马,   P : 64 }

---
+++ src/test/java/com/xiao/nettydemo/netty/c3/EventLoopServer.java
@@ -0,0 +1,50 @@
+package com.xiao.nettydemo.netty.c3;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+
+@Slf4j
+public class EventLoopServer {
+
+    public static void main(String[] args) {
+        // 细分2: 创建一个独立的 EventLoopGroup
+        EventLoopGroup group = new DefaultEventLoopGroup();
+        new ServerBootstrap()
+                // boos and worker
+
+                // 细分1: boss 只负责 accept 事件     worker 只负责 socketChannel 上的读写
+                //
+                .group(new NioEventLoopGroup(),new NioEventLoopGroup(2))
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        // ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast("handle1",new ChannelInboundHandlerAdapter(){
+                            @Override           // ByteBuf
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug(buf.toString(Charset.defaultCharset()));
+                                // 交给下一个handler
+                                ctx.fireChannelRead(msg); // 将消息传给下一个 handler
+                            }
+                        }).addLast(group,"handle2",new ChannelInboundHandlerAdapter(){
+                            @Override           // ByteBuf
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug(buf.toString(Charset.defaultCharset()));
+                            }
+                        });
+                    }
+                })
+                .bind(8080);
+    }
+
+}
---
+++ src/test/java/com/xiao/nettydemo/netty/c3/TestEventLoop.java
@@ -0,0 +1,46 @@
+package com.xiao.nettydemo.netty.c3;
+
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.util.NettyRuntime;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class TestEventLoop {
+
+    public static void main(String[] args) {
+        // 1. 创建事件循环组
+        EventLoopGroup group = new NioEventLoopGroup(2); // io 事件, 普通任务, 定时任务
+//        EventLoopGroup defaultEventLoop = new DefaultEventLoop(); //普通任务, 定时任务
+//        System.out.println(NettyRuntime.availableProcessors());
+        // 2. 获取下一个事件循环对象
+        log.debug("当前任务对象:{}",group.next());
+        log.debug("当前任务对象:{}",group.next());
+        log.debug("当前任务对象:{}",group.next());
+        log.debug("当前任务对象:{}",group.next());
+
+
+        // 3. 执行普通任务
+        // execute
+        /*group.next().submit(()->{
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            log.debug("ok");
+        });*/
+
+        // 4. 执行定时任务
+        group.next().scheduleAtFixedRate(()->{
+          log.debug("ok");
+        },0,1, TimeUnit.SECONDS);
+
+
+        log.debug("main");
+
+    }
+
+}
